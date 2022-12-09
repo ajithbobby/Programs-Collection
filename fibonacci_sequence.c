6875a69676dafe6593daa6ad4431fd860733a017
Ajith Bobby <ajithbobbyabk@gmail.com>
2022-12-09 21:19:25 +0530

    Update fibonacci_sequence.c

--- fibonacci_sequence.c
+++ fibonacci_sequence.c
@@ -1,30 +1,18 @@
-# include<stdio.h>
-
-int main(){
-    printf("%d", fibonacci());
-    return 0;
-}
-int fibonacci(){
-    int a, f=0, f1=0, f2=1;
-    printf("enter the number of terms : ");
-    scanf("%d", &a);
-   
-    for(int i=1;i<=a;i++){
-        if(i==1){
-            printf("%d\n",f1);
-        }
-        if(i==2){
-            printf("%d\n",f2);
-        }
-
-        f = f1 + f2;
-        f1 = f2;
-        f2 = f; 
-        printf("%d\n", f);
-    }
-        
-    
-}
+#include<stdio.h>    
+int main()    
+{int n1=0,n2=1,n3,i,number;    
+ printf("Enter the number of elements:");    
+ scanf("%d",&number);    
+ printf("\n%d %d",n1,n2);    
+ for(i=2;i<number;++i)   
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ }    
     
 
     
